2061c93e68c9f59b0da6a2c020aa8e30f6b0e4b7
eindiran <eindiran@uchicago.edu>
2021-02-04 23:55:38 -0800

    Clean up pattern matching code so that | is used instead of multiple arms

--- src/main.rs
+++ src/main.rs
@@ -29,73 +29,22 @@ use structopt::StructOpt;
  */
 fn to_length(input_length: f64, length_unit: String) -> Option<Length> {
     match length_unit.to_lowercase().trim().as_ref() {
-        // Miles
-        "mile"        => Some(Length::from_miles(input_length)),
-        "miles"       => Some(Length::from_miles(input_length)),
-        "mi"          => Some(Length::from_miles(input_length)),
-        // Furlongs
-        "furlong"     => Some(Length::from_furlongs(input_length)),
-        "furlongs"    => Some(Length::from_furlongs(input_length)),
-        "f"           => Some(Length::from_furlongs(input_length)),
-        "fur"         => Some(Length::from_furlongs(input_length)),
-        // Yards
-        "yard"        => Some(Length::from_yards(input_length)),
-        "yards"       => Some(Length::from_yards(input_length)),
-        "y"           => Some(Length::from_yards(input_length)),
-        "yd"          => Some(Length::from_yards(input_length)),
-        "yds"         => Some(Length::from_yards(input_length)),
-        // Feet
-        "feet"        => Some(Length::from_feet(input_length)),
-        "foot"        => Some(Length::from_feet(input_length)),
-        "ft"          => Some(Length::from_feet(input_length)),
-        "'"           => Some(Length::from_feet(input_length)),
-        // Inches
-        "inch"        => Some(Length::from_inches(input_length)),
-        "inches"      => Some(Length::from_inches(input_length)),
-        "i"           => Some(Length::from_inches(input_length)),
-        "in"          => Some(Length::from_inches(input_length)),
-        "\""          => Some(Length::from_inches(input_length)),
-        // Thou
-        "thou"        => Some(Length::from_inches(input_length / 1000f64)),
-        "th"          => Some(Length::from_inches(input_length / 1000f64)),
-        // Kilometers
-        "kilometer"   => Some(Length::from_kilometers(input_length)),
-        "kilometers"  => Some(Length::from_kilometers(input_length)),
-        "km"          => Some(Length::from_kilometers(input_length)),
-        // Hectometers
-        "hectometer"  => Some(Length::from_hectometers(input_length)),
-        "hectometers" => Some(Length::from_hectometers(input_length)),
-        "hm"          => Some(Length::from_hectometers(input_length)),
-        // Meters
-        "meter"       => Some(Length::from_meters(input_length)),
-        "meters"      => Some(Length::from_meters(input_length)),
-        "metre"       => Some(Length::from_meters(input_length)),
-        "metres"      => Some(Length::from_meters(input_length)),
-        "m"           => Some(Length::from_meters(input_length)),
-        // Decimeters
-        "decimeter"   => Some(Length::from_decimeters(input_length)),
-        "decimeters"  => Some(Length::from_decimeters(input_length)),
-        "decimetre"   => Some(Length::from_decimeters(input_length)),
-        "decimetres"  => Some(Length::from_decimeters(input_length)),
-        "dm"          => Some(Length::from_decimeters(input_length)),
-        // Centimeters
-        "centimeter"  => Some(Length::from_centimeters(input_length)),
-        "centimeters" => Some(Length::from_centimeters(input_length)),
-        "centimetre"  => Some(Length::from_centimeters(input_length)),
-        "centimetres" => Some(Length::from_centimeters(input_length)),
-        "cm"          => Some(Length::from_centimeters(input_length)),
-        // Millimeters
-        "millimeter"  => Some(Length::from_millimeters(input_length)),
-        "millimeters" => Some(Length::from_millimeters(input_length)),
-        "millimetre"  => Some(Length::from_millimeters(input_length)),
-        "millimetres" => Some(Length::from_millimeters(input_length)),
-        "mm"          => Some(Length::from_millimeters(input_length)),
-        // Nanometers
-        "nanometer"   => Some(Length::from_nanometers(input_length)),
-        "nanometers"  => Some(Length::from_nanometers(input_length)),
-        "nanometre"   => Some(Length::from_nanometers(input_length)),
-        "nanometres"  => Some(Length::from_nanometers(input_length)),
-        "nm"          => Some(Length::from_nanometers(input_length)),
+        /* IMPERIAL LENGTH UNITS: */
+        "mile" | "miles" | "mi"                                            => Some(Length::from_miles(input_length)),            // Miles
+        "furlong" | "furlongs" | "fur" | "furs"                            => Some(Length::from_furlongs(input_length)),         // Furlongs
+        "yard" | "yards" | "y" | "yd" | "yds"                              => Some(Length::from_yards(input_length)),            // Yards
+        "foot" | "feet" | "ft" | "'"                                       => Some(Length::from_feet(input_length)),             // Feet
+        "inch" | "inches" | "i" | "in" | "\""                              => Some(Length::from_inches(input_length)),           // Inches
+        "thou" | "thous" | "th"                                            => Some(Length::from_inches(input_length / 1000f64)), // Thou
+        /* METRIC LENGTH UNITS: */
+        "kilometer" | "kilometers" | "kilometre" | "kilometres" | "km"     => Some(Length::from_kilometers(input_length)),       // Kilometers
+        "hectometer" | "hectometers" | "hectometre" | "hectometres"| "hm"  => Some(Length::from_hectometers(input_length)),      // Hectometers
+        "meter" | "meters" | "metre" | "metres" | "m"                      => Some(Length::from_meters(input_length)),           // Meters
+        "decimeter" | "decimeters" | "decimetre" | "decimetres"| "dm"      => Some(Length::from_decimeters(input_length)),       // Decimeters
+        "centimeter" | "centimeters" | "centimetre" | "centimetres"| "cm"  => Some(Length::from_centimeters(input_length)),      // Centimeters
+        "millimeter" | "millimeters" | "millimetre" | "millimetres"| "mm"  => Some(Length::from_millimeters(input_length)),      // Millimeters
+        "nanometer" | "nanometers" | "nanometre" | "nanometres"| "nm"      => Some(Length::from_nanometers(input_length)),       // Nanometers
+        /* OTHER: */
         _           => { eprintln!("Unknown unit of input length: {}", length_unit); None }
     }
 }
@@ -116,73 +65,22 @@ fn from_length(length: Option<Length>, output_unit: String) -> Option<f64> {
         None     => None,
         Some(l)  => {
             match output_unit.to_lowercase().trim().as_ref() {
-                // Miles
-                "mile"        => Some(l.as_miles()),
-                "miles"       => Some(l.as_miles()),
-                "mi"          => Some(l.as_miles()),
-                // Furlongs
-                "furlong"     => Some(l.as_furlongs()),
-                "furlongs"    => Some(l.as_furlongs()),
-                "f"           => Some(l.as_furlongs()),
-                "fur"         => Some(l.as_furlongs()),
-                // Yards
-                "yard"        => Some(l.as_yards()),
-                "yards"       => Some(l.as_yards()),
-                "y"           => Some(l.as_yards()),
-                "yd"          => Some(l.as_yards()),
-                "yds"         => Some(l.as_yards()),
-                // Feet
-                "feet"        => Some(l.as_feet()),
-                "foot"        => Some(l.as_feet()),
-                "ft"          => Some(l.as_feet()),
-                "'"           => Some(l.as_feet()),
-                // Inches
-                "inch"        => Some(l.as_inches()),
-                "inches"      => Some(l.as_inches()),
-                "i"           => Some(l.as_inches()),
-                "in"          => Some(l.as_inches()),
-                "\""          => Some(l.as_inches()),
-                // Thou
-                "thou"        => Some(l.as_inches() / 1000f64),
-                "th"          => Some(l.as_inches() / 1000f64),
-                // Kilometers
-                "kilometer"   => Some(l.as_kilometers()),
-                "kilometers"  => Some(l.as_kilometers()),
-                "km"          => Some(l.as_kilometers()),
-                // Hectometers
-                "hectometer"  => Some(l.as_hectometers()),
-                "hectometers" => Some(l.as_hectometers()),
-                "hm"          => Some(l.as_hectometers()),
-                // Meters
-                "meter"       => Some(l.as_meters()),
-                "meters"      => Some(l.as_meters()),
-                "metre"       => Some(l.as_meters()),
-                "metres"      => Some(l.as_meters()),
-                "m"           => Some(l.as_meters()),
-                // Decimeters
-                "decimeter"   => Some(l.as_decimeters()),
-                "decimeters"  => Some(l.as_decimeters()),
-                "decimetre"   => Some(l.as_decimeters()),
-                "decimetres"  => Some(l.as_decimeters()),
-                "dm"          => Some(l.as_decimeters()),
-                // Centimeters
-                "centimeter"  => Some(l.as_centimeters()),
-                "centimeters" => Some(l.as_centimeters()),
-                "centimetre"  => Some(l.as_centimeters()),
-                "centimetres" => Some(l.as_centimeters()),
-                "cm"          => Some(l.as_centimeters()),
-                // Millimeters
-                "millimeter"  => Some(l.as_millimeters()),
-                "millimeters" => Some(l.as_millimeters()),
-                "millimetre"  => Some(l.as_millimeters()),
-                "millimetres" => Some(l.as_millimeters()),
-                "mm"          => Some(l.as_millimeters()),
-                // Nanometers
-                "nanometer"   => Some(l.as_nanometers()),
-                "nanometers"  => Some(l.as_nanometers()),
-                "nanometre"   => Some(l.as_nanometers()),
-                "nanometres"  => Some(l.as_nanometers()),
-                "nm"          => Some(l.as_nanometers()),
+                /* IMPERIAL LENGTH UNITS: */
+                "mile" | "miles" | "mi"                                            => Some(l.as_miles()),            // Miles
+                "furlong" | "furlongs" | "fur" | "furs"                            => Some(l.as_furlongs()),         // Furlongs
+                "yard" | "yards" | "y" | "yd" | "yds"                              => Some(l.as_yards()),            // Yards
+                "foot" | "feet" | "ft" | "'"                                       => Some(l.as_feet()),             // Feet
+                "inch" | "inches" | "i" | "in" | "\""                              => Some(l.as_inches()),           // Inches
+                "thou" | "thous" | "th"                                            => Some(l.as_inches() / 1000f64), // Thou
+                /* METRIC LENGTH UNITS: */
+                "kilometer" | "kilometers" | "kilometre" | "kilometres" | "km"     => Some(l.as_kilometers()),       // Kilometers
+                "hectometer" | "hectometers" | "hectometre" | "hectometres"| "hm"  => Some(l.as_hectometers()),      // Hectometers
+                "meter" | "meters" | "metre" | "metres" | "m"                      => Some(l.as_meters()),           // Meters
+                "decimeter" | "decimeters" | "decimetre" | "decimetres"| "dm"      => Some(l.as_decimeters()),       // Decimeters
+                "centimeter" | "centimeters" | "centimetre" | "centimetres"| "cm"  => Some(l.as_centimeters()),      // Centimeters
+                "millimeter" | "millimeters" | "millimetre" | "millimetres"| "mm"  => Some(l.as_millimeters()),      // Millimeters
+                "nanometer" | "nanometers" | "nanometre" | "nanometres"| "nm"      => Some(l.as_nanometers()),       // Nanometers
+                /* OTHER: */
                 _           => { eprintln!("Unknown unit of output length: {}", output_unit); None }
             }
         }
